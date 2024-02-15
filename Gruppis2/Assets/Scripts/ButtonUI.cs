04d69f87be73124e874c83a6c003b0e94a49b9f2
᠋⁧⁧᠋⁧ <rubenschweder@gmail.com>
2024-02-15 10:19:55 +0100

    MenyÄndringar

--- Gruppis2/Assets/Scripts/ButtonUI.cs
+++ Gruppis2/Assets/Scripts/ButtonUI.cs
@@ -7,7 +7,8 @@ public class ButtonUI : MonoBehaviour
 {
     [SerializeField] private string Menu = "MainMenu";
     [SerializeField] private string Options = "Options";
-    [SerializeField] private string newGame = "Game";
+    [SerializeField] private string newGame = "Station1";
+    [SerializeField] private string Credits = "Credits";
 
 
 
@@ -15,6 +16,8 @@ public class ButtonUI : MonoBehaviour
     public void NewGameButton()
     {
         SceneManager.LoadScene(newGame);
+        PauseMenu.gameIsPaused = false;
+        
     }
 
     public void OptionsButton()
@@ -41,6 +44,10 @@ public class ButtonUI : MonoBehaviour
 
         }
 
+    }
+    public void CreditsScreen()
+    {
+        SceneManager.LoadScene(Credits);
     }
 
 
