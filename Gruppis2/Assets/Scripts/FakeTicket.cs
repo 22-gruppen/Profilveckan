1b4ac50f5f79c0f0df0b534169aea4ad0e90b6eb
bevern0 <lias.castorjonsson@elev.ga.lbs.se>
2024-02-29 10:09:00 +0100

    tICKETS

--- Gruppis2/Assets/Scripts/FakeTicket.cs
+++ Gruppis2/Assets/Scripts/FakeTicket.cs
@@ -8,54 +8,46 @@ public class FakeTicket : MonoBehaviour
     private bool isMouseOver = false;
     private bool canPickUp = false;
     public GameObject pickText;
-    public GameObject Count; 
+    public GameObject FakeText;
 
 
-    private int objectCount = 0; // Variable to keep track of the object count
+    private void Start()
+    {
+        pickText.SetActive(false);
+        FakeText.SetActive(false); 
+    }
 
     private void OnGUI()
     {
-        if (isMouseOver)
-        {
-
-            pickText.SetActive(true); 
-
-        }
+     
         if (isMouseOver && canPickUp && Input.GetKeyDown(KeyCode.E))
         {
-            
-            UpdateObjectCount(); // Update object count
+            pickText.SetActive(false);
+            FakeText.SetActive(true);
         }
+
     }
 
     private void Update()
     {
-        if (isMouseOver && canPickUp && Input.GetKeyDown(KeyCode.E))
-        {
-            UpdateObjectCount(); // Update object count
-        }
+       
     }
 
     private void OnMouseEnter()
     {
         isMouseOver = true;
         pickText.SetActive(true); 
+        
         canPickUp = true; // Set canPickUp to true when mouse is over the object
     }
 
     private void OnMouseExit()
     {
         isMouseOver = false;
-        pickText.SetActive(false); 
+        pickText.SetActive(false);
+        FakeText.SetActive(false);
         canPickUp = false; // Reset canPickUp when mouse exits the object
     }
 
-    private void UpdateObjectCount()
-    {
-        // Increment the count and update the text
-        objectCount++; // Increment the count
-        objectCount = Mathf.Clamp(objectCount, 0, 5); // Ensure the count stays between 0 and 5
-
-
-    }
+    
 }
